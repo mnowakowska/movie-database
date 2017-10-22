527598baac21fe3b289e7ac1e47a2a94ac898e9a
Malwina Nowakowska <malwina.nowakowskaa@gmail.com>
2017-10-23 00:10:30 +0200

    Add tests for App component

--- movie_browser/src/components/App/App.test.js
+++ movie_browser/src/components/App/App.test.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import LogOutRedux from './../LogOut/LogOut.Redux.js';
+
+
+describe('App Component', () => {
+    it('renders nav component component', () => {
+        const wrapper = shallow(<App isAuthenticated={true} />);
+        expect(wrapper.find('#browser-navbar').exists()).toBeTruthy();
+    });
+
+    it('renders logout link for log in users', () => {
+        const wrapper = shallow(<App isAuthenticated={true} />);
+        expect(wrapper.find(LogOutRedux).exists()).toBeTruthy();
+    });
+
+    it('not renders logout link for log in users', () => {
+        const wrapper = shallow(<App isAuthenticated={false} />);
+        expect(wrapper.find(LogOutRedux).exists()).toBeFalsy();
+    });
+
+    it('renders two links for log in users', () => {
+        const wrapper = shallow(<App isAuthenticated={true} />);
+        expect(wrapper.find('.nav-link').length).toBe(2);
+    });
+
+    it('renders one link for not log in users', () => {
+        const wrapper = shallow(<App isAuthenticated={false} />);
+        expect(wrapper.find('.nav-link').length).toBe(1);
+    });
 });
